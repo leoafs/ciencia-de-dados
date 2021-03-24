f6070c1279b68123184a7cc533efe14c3bde2256
Leonardo Alexandre <32283837+leoafs@users.noreply.github.com>
2021-03-23 22:34:24 -0300

    Update q4.py

--- q4.py
+++ q4.py
@@ -2,8 +2,8 @@ from random import randint
 import statistics
 def V1 ():
     v1 = []
-    for i in range(100):
-        v1.append(randint(0, 100))
+    for i in range(20):
+        v1.append(randint(0, 10))
     a=statistics.mean(v1)
     b=statistics.median(v1)
     if a>b:
@@ -12,7 +12,7 @@ def V1 ():
     return v1
 def V2 ():
     v2 = []
-    for i in range(10):
+    for i in range(20):
         v2.append(randint(0, 10))
     a=statistics.mean(v2)
     b=statistics.median(v2)
@@ -24,5 +24,7 @@ def V2 ():
 
 v1=V1()
 v2=V2()
+print("v1= ",v1)
 print("Média_v1",statistics.mean(v1),'>',"Mediana_v1",statistics.median(v1))
+print("v2= ",v2)
 print("Média_v2",statistics.mean(v2),'<',"Mediana_v2",statistics.median(v2))
